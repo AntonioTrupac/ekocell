6e78fa9fcf7a44cc94c2c92c9f8fe1ab64512d30
antonio.trupac <sniper.trupac@gmail.com>
2022-10-07 22:57:41 +0200

    anchor tag styling

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: "#94D237",
+				secondary: "#422D93",
+				tertiary: "#5e41d2",
+				accent: "#709327",
+				accent2: "#d26937",
+			}
+		},
 	},
 	plugins: [],
 }
